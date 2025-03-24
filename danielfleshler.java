23a4cd344163c71c9bddf9102e8d1781daf7db6f
danielfleshler <daniel.fle@my.hit.ac.il>
2025-03-24 11:09:44 +0200

    commit

--- danielfleshler.java
+++ danielfleshler.java
@@ -1,7 +1,7 @@
 public class DanielFleshler {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
-            System.out.println("Daniel Fleshler - Before Modifying" + i);
+            System.out.println("Daniel Fleshler - After Modifying" + i);
         }
     }
 }
